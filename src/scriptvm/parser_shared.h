7230c8a01df198f48f7efbcb150d9de85d9a4043
schoenebeck <schoenebeck@d2c4447f-3961-4fff-bec3-96d84be9abe5>
2020-01-02 16:09:58 +0000

    Public year bump to 2020.
    git-svn-id: https://svn.linuxsampler.org/svn/linuxsampler/trunk@3683 d2c4447f-3961-4fff-bec3-96d84be9abe5

---
+++ src/scriptvm/parser_shared.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2014-2019 Christian Schoenebeck
+ *
+ * http://www.linuxsampler.org
+ *
+ * This file is part of LinuxSampler and released under the same terms.
+ * See README file for details.
+ */
+
+// types shared between auto generated lexer and parser ...
+
+#ifndef LS_INSTRSCRIPTSPARSER_SHARED_H
+#define LS_INSTRSCRIPTSPARSER_SHARED_H
+
+#include <stdio.h>
+#include "tree.h"
+
+#if AC_APPLE_UNIVERSAL_BUILD
+# include "parser.tab.h"
+#else
+# include "parser.h"
+#endif
+
+#include "../common/global_private.h"
+    
+struct _YYSTYPE {
+    union {
+        LinuxSampler::vmint iValue;
+        LinuxSampler::vmfloat fValue;
+        char* sValue;
+        struct {
+            LinuxSampler::vmint iValue;
+            LinuxSampler::MetricPrefix_t prefix[2];
+            LinuxSampler::StdUnit_t unit;
+        } iUnitValue;
+        struct {
+            LinuxSampler::vmfloat fValue;
+            LinuxSampler::MetricPrefix_t prefix[2];
+            LinuxSampler::StdUnit_t unit;
+        } fUnitValue;
+    };
+    LinuxSampler::EventHandlersRef nEventHandlers;
+    LinuxSampler::EventHandlerRef nEventHandler;
+    LinuxSampler::StatementsRef nStatements;
+    LinuxSampler::StatementRef nStatement;
+    LinuxSampler::FunctionCallRef nFunctionCall;
+    LinuxSampler::ArgsRef nArgs;
+    LinuxSampler::ExpressionRef nExpression;
+    LinuxSampler::CaseBranch nCaseBranch;
+    LinuxSampler::CaseBranches nCaseBranches;
+};
+#define YYSTYPE _YYSTYPE
+#define yystype YYSTYPE     ///< For backward compatibility.
+#ifndef YYSTYPE_IS_DECLARED
+# define YYSTYPE_IS_DECLARED ///< We tell the lexer / parser that we use our own data structure as defined above.
+#endif
+
+#endif // LS_INSTRSCRIPTSPARSER_SHARED_H
